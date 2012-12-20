1d5da0063551798c43ee4f7491f7972436c5a3d5
Chris Price <chris@puppetlabs.com>
2012-12-19 20:42:45 -0800

    Initial working (hopefully) implementation of generating all master certs

--- src/test/java/com/puppetlabs/sandbox/SslTest.java
+++ src/test/java/com/puppetlabs/sandbox/SslTest.java
@@ -10,9 +10,13 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x509.*;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMReader;
 import org.bouncycastle.openssl.PEMWriter;
+import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
@@ -24,6 +28,9 @@ import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Period;
 import org.junit.Test;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -35,22 +42,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.math.BigInteger;
 import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.fail;
 
 public class SslTest {
 
-    private static final String PATH_CA_PASSWORD = "./target/test/master/conf/ssl/ca/private/ca.pass";
-    private static final String PATH_CA_CERT = "./target/test/master/conf/ssl/ca/ca_crt.pem";
+    private static final String PATH_TEST_DIR = "./target/test/master/conf/ssl";
+//    private static final String PATH_CA_PASSWORD = "./target/test/master/conf/ssl/ca/private/ca.pass";
+    private static final String PATH_CA_PUBLIC_KEY = PATH_TEST_DIR + "/ca/ca_pub.pem";
+    private static final String PATH_CA_PRIVATE_KEY = PATH_TEST_DIR + "/ca/ca_key.pem";
+    private static final String PATH_CA_CERT = PATH_TEST_DIR + "/ca/ca_crt.pem";
     // TODO: don't hard-code localhost
-    private static final String PATH_MASTER_CERT = "./target/master/conf/ssl/certs/localhost.pem";
-    private static final String PATH_MASTER_PRIVATE_KEY = "./target/test/master/conf/ssl/private_keys/localhost.pem";
-    private static final String PATH_MASTER_PUBLIC_KEY = "./target/test/master/conf/ssl/public_keys/localhost.pem";
+    private static final String PATH_MASTER_PUBLIC_KEY = PATH_TEST_DIR + "/public_keys/localhost.pem";
+    private static final String PATH_MASTER_PRIVATE_KEY = PATH_TEST_DIR + "/private_keys/localhost.pem";
+    private static final String PATH_MASTER_CERT = PATH_TEST_DIR + "/certs/localhost.pem";
+
+    private static final AtomicInteger nextSerialNum = new AtomicInteger(1);
 
     public class HelloServlet extends HttpServlet
     {
@@ -116,7 +130,7 @@ public class SslTest {
 
         Connector connector = new SslSelectChannelConnector(sslContextFactory);
 //        Connector connector = new SslSelectChannelConnector();
-        connector.setPort(8081);
+        connector.setPort(8140);
         server.addConnector(connector);
 
 
@@ -155,80 +169,102 @@ public class SslTest {
     }
 
 
-    private void createMasterCerts() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, OperatorCreationException {
-        FileUtils.forceMkdir(new File(PATH_CA_PASSWORD).getParentFile());
-        FileUtils.forceMkdir(new File(PATH_MASTER_PRIVATE_KEY).getParentFile());
-        FileUtils.forceMkdir(new File(PATH_MASTER_PUBLIC_KEY).getParentFile());
+    private void createMasterCerts() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, OperatorCreationException, CertificateException {
+        for (String filePath : new String[] {
+                PATH_CA_CERT, PATH_CA_PRIVATE_KEY, PATH_CA_PUBLIC_KEY,
+                PATH_MASTER_CERT, PATH_MASTER_PRIVATE_KEY, PATH_MASTER_PUBLIC_KEY
+        }) {
+            FileUtils.forceMkdir(new File(filePath).getParentFile());
+        }
 
-        saveCAPassword(generateCAPassword());
-        KeyPair keyPair = generateKeyPair();
-        saveKeyPair(keyPair);
+        // TODO: it doesn't look like the CA password file is ever actually
+        // used for anything in Puppet.
+//        saveCAPassword(generateCAPassword());
+
+        KeyPair caKeyPair = generateKeyPair();
+        saveToPEM(caKeyPair.getPublic(), PATH_CA_PUBLIC_KEY);
+        saveToPEM(caKeyPair.getPrivate(), PATH_CA_PRIVATE_KEY);
 
         // TODO: don't hard-code localhost
-        PKCS10CertificationRequest certReq = generateCertReq(keyPair, "Puppet CA: localhost");
-        X509Certificate cert = signCertificateRequest(certReq, keyPair.getPrivate());
-        saveCACert(cert);
+        String host = "localhost";
 
-//        # Create a new cert request.  We do this specially, because we don't want
-//        # to actually save the request anywhere.
-//        request = Puppet::SSL::CertificateRequest.new(host.name)
-//
-//        # We deliberately do not put any subjectAltName in here: the CA
-//        # certificate absolutely does not need them. --daniel 2011-10-13
-//        request.generate(host.key)
-//
-//        # Create a self-signed certificate.
-//        @certificate = sign(host.name, false, request)
-//
+        X500Name caName = generateX500Name("Puppet CA: " + host);
+
+        PKCS10CertificationRequest caCertReq = generateCertReq(caKeyPair, caName);
+        X509Certificate caCert = signCertificateRequest(caCertReq, caName, caKeyPair.getPrivate());
+        saveToPEM(caCert, PATH_CA_CERT);
+
+        KeyPair hostKeyPair = generateKeyPair();
+        saveToPEM(hostKeyPair.getPublic(), PATH_MASTER_PUBLIC_KEY);
+        saveToPEM(hostKeyPair.getPrivate(), PATH_MASTER_PRIVATE_KEY);
+
+        X500Name hostName = generateX500Name(host);
+
+        PKCS10CertificationRequest hostCertReq = generateCertReq(hostKeyPair, hostName);
+        X509Certificate hostCert = signCertificateRequest(hostCertReq, caName, caKeyPair.getPrivate());
+        saveToPEM(hostCert, PATH_MASTER_CERT);
+
+        // TODO: crl
 //        # And make sure we initialize our CRL.
 //                crl
-        fail();
     }
 
-    private String generateCAPassword() {
-        StringBuilder sb = new StringBuilder();
-        Random rand = new Random();
-        for (int i = 0; i < 20; i++) {
-            sb.append((char)(rand.nextInt(74) + 48));
-        }
-        return sb.toString();
+    // TODO: It really sucks that the most specific type we can use
+    //  here is 'Object'.
+    private void saveToPEM(Object pemObject, String filePath) throws IOException {
+        PEMWriter pemWriter = new PEMWriter(new FileWriter(filePath));
+        pemWriter.writeObject(pemObject);
+        pemWriter.flush();
     }
 
-    private void saveCAPassword(String caPass) throws IOException {
-        FileUtils.writeStringToFile(new File(PATH_CA_PASSWORD), caPass, "UTF-8");
-    }
+
+//    private String generateCAPassword() {
+//        StringBuilder sb = new StringBuilder();
+//        Random rand = new Random();
+//        for (int i = 0; i < 20; i++) {
+//            sb.append((char)(rand.nextInt(74) + 48));
+//        }
+//        return sb.toString();
+//    }
+//
+//    private void saveCAPassword(String caPass) throws IOException {
+//        FileUtils.writeStringToFile(new File(PATH_CA_PASSWORD), caPass, "UTF-8");
+//    }
 
     private KeyPair generateKeyPair()
             throws NoSuchAlgorithmException, NoSuchProviderException
     {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "BC");
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", BouncyCastleProvider.PROVIDER_NAME);
         keyGen.initialize(4096);
         KeyPair keyPair = keyGen.generateKeyPair();
 
         return keyPair;
     }
 
-    private void saveKeyPair(KeyPair keyPair) throws IOException {
-        PEMWriter pemWriter = new PEMWriter(new FileWriter(PATH_MASTER_PRIVATE_KEY));
-        pemWriter.writeObject(keyPair.getPrivate());
-        pemWriter.flush();
+//    private void saveKeyPair(KeyPair keyPair) throws IOException {
+//        PEMWriter pemWriter = new PEMWriter(new FileWriter(PATH_MASTER_PRIVATE_KEY));
+//        pemWriter.writeObject(keyPair.getPrivate());
+//        pemWriter.flush();
+//
+//        pemWriter = new PEMWriter(new FileWriter(PATH_MASTER_PUBLIC_KEY));
+//        pemWriter.writeObject(keyPair.getPublic());
+//        pemWriter.flush();
+//    }
 
-        pemWriter = new PEMWriter(new FileWriter(PATH_MASTER_PUBLIC_KEY));
-        pemWriter.writeObject(keyPair.getPublic());
-        pemWriter.flush();
-    }
-
-
-    private PKCS10CertificationRequest generateCertReq(KeyPair keyPair, String commonName) throws OperatorCreationException, IOException {
+    private X500Name generateX500Name(String commonName) {
         X500NameBuilder x500NameBuilder = new X500NameBuilder(BCStyle.INSTANCE);
         x500NameBuilder.addRDN(BCStyle.CN, commonName);
 
-        X500Name x500Name = x500NameBuilder.build();
+        return x500NameBuilder.build();
+    }
 
+
+
+    private PKCS10CertificationRequest generateCertReq(KeyPair keyPair, X500Name subjectName) throws OperatorCreationException, IOException {
         // TODO: the puppet code sets a property "version=0" on the request object
-        //  here; can't figure out how to do that at the moment.
+        //  here; can't figure out how to do that at the moment.  Not sure if it's needed.
         PKCS10CertificationRequestBuilder requestBuilder =
-                new JcaPKCS10CertificationRequestBuilder(x500Name, keyPair.getPublic());
+                new JcaPKCS10CertificationRequestBuilder(subjectName, keyPair.getPublic());
 
         // TODO: support DNS ALT names; probably looks something like this:
 //        Extensions extensions = new Extensions(new Extension[] {
@@ -249,13 +285,55 @@ public class SslTest {
         return request;
     }
 
-    private X509Certificate signCertificateRequest(PKCS10CertificationRequest certReq, PrivateKey privateKey) {
-        throw new NotImplementedException();
+
+    private X509Certificate signCertificateRequest(PKCS10CertificationRequest certReq,
+                                                   X500Name issuer,
+                                                   PrivateKey issuerPrivateKey)
+            throws OperatorCreationException, CertificateException {
+
+//        # Make the certificate valid as of yesterday, because so many people's
+//        # clocks are out of sync.  This gives one more day of validity than people
+//        # might expect, but is better than making every person who has a messed up
+//        # clock fail, and better than having every cert we generate expire a day
+//        # before the user expected it to when they asked for "one year".
+        DateTime notBefore = DateTime.now().minus(Period.days(1));
+        DateTime notAfter = DateTime.now().plus(Period.years(5));
+
+        X509v3CertificateBuilder builder = new X509v3CertificateBuilder(
+                issuer,
+                nextSerial(),
+                notBefore.toDate(),
+                notAfter.toDate(),
+                certReq.getSubject(),
+                certReq.getSubjectPublicKeyInfo());
+
+        // TODO: add extensions to cert (maps to build_ca_extensions,
+        //  build_server_extensions in certificate_factory.rb.
+//
+//        add_extensions_to(cert, csr, issuer, send(build_extensions))
+//
+
+        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder("SHA256WithRSA");
+        signerBuilder.setProvider(BouncyCastleProvider.PROVIDER_NAME);
+        ContentSigner signer = signerBuilder.build(issuerPrivateKey);
+
+        X509CertificateHolder holder = builder.build(signer);
+        JcaX509CertificateConverter converter = new JcaX509CertificateConverter();
+        converter.setProvider(BouncyCastleProvider.PROVIDER_NAME);
+        return converter.getCertificate(holder);
     }
 
-    private void saveCACert(X509Certificate cert) {
-        throw new NotImplementedException();
+    private BigInteger nextSerial() {
+        // TODO: this needs to be able to persist between runs.
+        int val = nextSerialNum.getAndIncrement();
+        return BigInteger.valueOf(val);
     }
+
+//    private void saveCACert(X509Certificate cert) throws IOException {
+//        PEMWriter pemWriter = new PEMWriter(new FileWriter(PATH_CA_CERT));
+//        pemWriter.writeObject(cert);
+//        pemWriter.flush();
+//    }
 
 
     private KeyStore createMasterKeyStore() throws IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
